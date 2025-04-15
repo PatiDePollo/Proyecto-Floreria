b128df57e29f0541d734f30581404fea7012efc4
PatiDePollo <103049904+PatiDePollo@users.noreply.github.com>
2025-04-15 15:46:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Aquí puedes manejar eventos, como enviar formularios o alertas de compra
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("¡Su solicitud de contacto se ha enviado!");
+  });
+  
