1a0498d57862c4af4053465cbe7e7d0843934d76
PatiDePollo <103049904+PatiDePollo@users.noreply.github.com>
2025-05-05 18:33:51 -0300

    Update script.js
    Subida de JS2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
   const form = new FormData(this);
-  fetch('enviar_mail.php', {
+  fetch('https://panchosrv.bringfeel.com.ar/florencioelias/7mo3ra/Moyano/php/enviar_mail.php', {
     method: 'POST',
     body: form
   })
   .then(response => response.text())
   .then(data => {
     document.getElementById('respuesta').innerText = data;
-    this.reset(); // Limpia el formulario
+    this.reset();
   })
   .catch(error => {
     document.getElementById('respuesta').innerText = 'Error al enviar el mensaje.';
